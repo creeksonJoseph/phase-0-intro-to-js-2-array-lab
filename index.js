4c3fd28503855a30f083323956e7ce548fca94fa
creeksonjoseph <charanajoseph@gmail.com>
2025-06-12 09:15:57 +0300

    methds

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(Ralph) {
+  cats.push(Ralph);
+  return cats;
+}
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob);
+  return cats;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+function appendCat(Broom) {
+  return [...cats, Broom];
+}
+function prependCat(Arnold) {
+  return [Arnold, ...cats];
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
